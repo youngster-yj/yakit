d5e91e0f95f35544e5af93602cef109b1c2eeae4
TangHang123 <1425735414@qq.com>
2023-02-02 11:32:33 +0800

    添加后自动关闭

--- app/renderer/src/main/src/pages/yakitStore/PluginOperator.tsx
+++ app/renderer/src/main/src/pages/yakitStore/PluginOperator.tsx
@@ -637,6 +637,7 @@ export interface AddToMenuActionFormProp {
     script: YakScript
     visible: boolean
     updateGroups?: () => any
+    setVisible: (b: boolean) => any
 }
 
 interface OptionsProps {
@@ -654,7 +655,7 @@ interface AddToMenuRequest {
 
 export const AddToMenuActionForm: React.FC<AddToMenuActionFormProp> = (props) => {
     const [form] = Form.useForm()
-    const {script, visible} = props
+    const {script, visible, setVisible} = props
     const updateGroups = props?.updateGroups ? props.updateGroups : () => {}
     const [patternMenu, setPatternMenu] = useState<"expert" | "new">("expert")
     const [menuData, setMenuData] = useState<MenuItemGroup[]>([])
@@ -731,6 +732,7 @@ export const AddToMenuActionForm: React.FC<AddToMenuActionFormProp> = (props) =>
                         .then(() => {
                             ipcRenderer.invoke("change-main-menu")
                             updateGroups()
+                            setVisible(false)
                             success("添加成功")
                         })
                         .catch((e: any) => {
@@ -785,11 +787,17 @@ export const PluginManagement: React.FC<PluginManagementProps> = React.memo<Plug
                 content={
                     <>
                         {script && (
-                            <AddToMenuActionForm visible={visibleAdd} script={script} updateGroups={updateGroups} />
+                            <AddToMenuActionForm
+                                visible={visibleAdd}
+                                setVisible={setVisibleAdd}
+                                script={script}
+                                updateGroups={updateGroups}
+                            />
                         )}
                     </>
                 }
                 trigger={["click"]}
+                visible={visibleAdd}
                 onVisibleChange={(visible) => {
                     setVisibleAdd(visible)
                 }}
